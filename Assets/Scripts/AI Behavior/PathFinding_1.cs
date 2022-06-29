2a8769990008db4d05fea6dbf536962a44c77b52
Aaron Occhipinti <aocchipinti99@gmail.com>
2022-06-28 23:04:36 -0400

    PathFindingTest1

--- Assets/Scripts/AI Behavior/PathFinding_1.cs
+++ Assets/Scripts/AI Behavior/PathFinding_1.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PathFinding_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb; //main rigidbody
+                            //    public Animator walkAnimator; //animator for implementing walk animations
+    public Animator walkAnimator;
+    private Vector2 movement;
+    public float moveSpeed;
+    public int speed;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        speed = 1;
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        movement.x = player.transform.position.x - transform.position.x;
+        movement.y = player.transform.position.y - transform.position.y;
+        // rb.MovePosition(this.transform.position);
+        rb.MovePosition(new Vector2((transform.position.x + speed * movement.x * Time.deltaTime), (transform.position.y + speed * movement.y * Time.deltaTime)));
     }
 }
